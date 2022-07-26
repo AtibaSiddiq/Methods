5ee977585f325343555e63add01dba75194d0a75
SBM2009 <atibasiddiq@gmail.com>
2022-07-26 09:39:40 +0100

    commit methods

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        // {
+            // double length = 456;
+            // double width = 76.7d;
+            // double second = 76.7d;
+            // double area = 55.7;
+            // Console.WriteLine(CalculateAreaOfRectangle(length, width));
+            // Console.WriteLine(CalculateVolumeOfRectangle(area, second));
+            // CheckIfEqual();
+        // }
+        // public static double CalculateAreaOfRectangle(double length, double width)
+        // {
+        //     double area = length * width;
+        //     return area;
+        // } 
+        // public static double CalculateVolumeOfRectangle(double area, double second)
+        // {
+        //     double volume = area * second;
+        //     return volume;
+        // } 
+        // public static void CheckIfEqual()
+        // {
+        //     double length = 456;
+        //     double width = 76.7;
+        //     double second = 76.7;
+        //     double area = 55.7;
+        //     if(CalculateAreaOfRectangle(length, width) == CalculateVolumeOfRectangle(area, second))
+        //     {
+        //         Console.WriteLine("They are equal");
+        //     }
+        //     else if(CalculateAreaOfRectangle(length, width) > CalculateVolumeOfRectangle(area, second))
+        //     {
+        //         Console.WriteLine("The area of the rectangle is greater than the volume of the rectangle");
+        //     }
+        //     else if (CalculateAreaOfRectangle(length, width) < CalculateVolumeOfRectangle(area, second))
+        //     {
+        //         Console.WriteLine("The volume of the rectangle is greater than the area of the rectangle");
+        //     }
+        // }   
+        // static void Main(string[] args)
+        // {
+        //     double length = 2;
+        //     double width = 6;
+        //     int weigth  = 3;
+        //     Console.WriteLine("The first area is: {0} " ,GetArea(length, width));
+        //     Console.WriteLine("The second area is: {0}" ,GetArea(length, weigth));
+        // }  
+        // public static double GetArea(double length, double width)
+        // {
+        //     double area = length * width;
+        //     return area;
+        // }
+        // public static double GetArea(double length, int weigth)
+        // {
+        //     double area = length * weigth;
+        //     return area;
+
+        // }
+        static void Main(string[] args)
+        {
+            
+        }
+       
+       
+    }
+}
